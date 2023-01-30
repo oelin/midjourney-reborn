b186d157b94a413849d5ee50bbdc6332b05e4a78
oelin <42823429+oelin@users.noreply.github.com>
2023-01-30 12:16:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const { token } = require('./config.json')
+
+const path = require('path')
+const fs = require('fs')
+
+
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+	],
+})
+
+
+client.commands = new Collection()
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+client.once(Events.ClientReady, ({ user }) => {
+	console.log(`Client ready! Logged in as ${user.tag}.`)
+})
+
+
+
+
+client.login(token)
